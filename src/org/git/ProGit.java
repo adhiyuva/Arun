da6d527b3b5263b10dbebc156ba02b53f0fa4100
adhiyuva <thejaadhi775@gmail.com>
2022-03-26 20:08:16 +0530

    hello

---
+++ src/org/git/ProGit.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class ProGit {
+	public void add() {
+		System.out.println("add");
+		}
+	public static void main(String[] args) {
+		ProGit pro = new ProGit();
+		pro.add();
+	}
+
+}
